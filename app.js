f34bc0c6042f12e65048b4763ac0a37ae0a3f9e3
bwapes <benwaples@gmail.com>
2020-12-17 12:41:48 -0800

    countdown logic

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
 // import functions and grab DOM elements
-import { computersGuess, resetGame } from './utils.js';
+import { computersGuess } from './utils.js';
 const result = document.querySelector('.result');
 const button = document.querySelector('.button');
 const score = document.querySelector('.score');
-console.log('test');
+const response = document.querySelector('#response');
+const displayGuesses = document.querySelector('.guesses');
+
 // initialize state
+
 let randomNumber = computersGuess();
 let totalPlayed = 0;
 let totalWon = 0;
 let remainingGuesses = 4;
+
 // set event listeners to update state and DOM
+
 button.addEventListener('click', () => {
+    response.src = '';
     const playersGuess = Number(document.querySelector('#guess').value);
-    console.log('players guess', playersGuess);
     console.log('computer guess', randomNumber);
 
     if (playersGuess === randomNumber) {
@@ -20,15 +25,23 @@ button.addEventListener('click', () => {
         totalWon++;
         totalPlayed++;
         remainingGuesses = 4;
-        resetGame(result, remainingGuesses, totalPlayed);
         randomNumber = computersGuess();
+        response.src = 'https://media.giphy.com/media/o75ajIFH0QnQC3nCeD/giphy.gif';
     } else if (playersGuess > randomNumber) {
         result.textContent = 'too high';
+        remainingGuesses--;
     } else if (playersGuess < randomNumber) {
         result.textContent = 'too low';
+        remainingGuesses--;
     } 
 
-    remainingGuesses === 0 && (randomNumber = resetGame(result, remainingGuesses, totalPlayed));
-
+    if (remainingGuesses === 0) {
+        result.textContent = 'You lose';
+        totalPlayed++;
+        remainingGuesses = 4;
+        randomNumber = computersGuess();
+        response.src = 'https://media.giphy.com/media/3ePb1CHEjfSRhn6r3c/giphy.gif';
+    }
+    displayGuesses.textContent = `${remainingGuesses}`;
     score.textContent = `${totalWon}/${totalPlayed}`;
 });
--- utils.js
+++ utils.js
@@ -1,9 +1,3 @@
 export function computersGuess() {
     return Math.ceil(Math.random() * 20);
 }
-
-export function resetGame(result, remainingGuesses, totalPlayed) {
-    result.textContent = 'you lose';
-    remainingGuesses = 4;
-    totalPlayed++;
-}
